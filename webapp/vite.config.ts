126565c8f0372afd514878f1efd80862d6a29482
hisayuki_mori <h.mori0206@gmail.com>
2022-01-10 21:57:08 +0900

    vitestのインストール

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />  // 追加
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    global: true,
+    environment: 'jsdom'
+  }
 })
