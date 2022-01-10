210aaf99142c60802574d1ebc6dda39ea556d216
hisayuki_mori <h.mori0206@gmail.com>
2022-01-11 02:08:07 +0900

    port指定して無限リローディングを回避

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -5,6 +5,13 @@ import WindiCSS from 'vite-plugin-windicss' // 追加
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    fs: {
+      strict: false
+    }
+  },
   plugins: [react(), WindiCSS()],
   test: {
     global: true,
