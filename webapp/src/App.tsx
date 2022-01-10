350e92ba0980441f5f6507bb8ad7171950db54f0
hisayuki_mori <h.mori0206@gmail.com>
2022-01-11 01:28:47 +0900

    Windi CSSの追加

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,41 +3,22 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
+    <div className="text-center my-8 flex justify-center">
+      <div className="px-8 py-6 rounded-2xl bg-gray-600 text-gray-100">
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
+            rel="noreferrer"
+            href="https://github.com/suzukey/vite_windi_sample"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
+            Vite + React + WindiCSS
           </a>
         </p>
-      </header>
+        <p>
+          <em className="text-sm opacity-75">Soooo Fast!</em>
+        </p>
+      </div>
     </div>
   )
 }
--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />  // 追加
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import WindiCSS from 'vite-plugin-windicss' // 追加
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), WindiCSS()],
   test: {
     global: true,
     environment: 'jsdom'
