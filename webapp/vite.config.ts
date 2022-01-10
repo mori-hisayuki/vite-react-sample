8355042c0792d7138de43e20e7fe323f511ee409
hisayuki_mori <h.mori0206@gmail.com>
2022-01-11 02:22:50 +0900

    hostフラグの代わりにconfigで指定

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -6,11 +6,7 @@ import WindiCSS from 'vite-plugin-windicss' // 追加
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
-    port: 3000,
-    fs: {
-      strict: false
-    }
+    host: true
   },
   plugins: [react(), WindiCSS()],
   test: {
